9576fb289cb945af215414047fb54a8ed46d345f
monsieurluge <monsieurluge@gmail.com>
2022-11-05 13:53:52 +0100

    test: disable cypress screenshots & videos outputs

--- cypress.config.js
+++ cypress.config.js
@@ -5,8 +5,10 @@ module.exports = defineConfig({
         baseUrl: 'http://localhost:1235',
         downloadsFolder: 'tests/output/cypress-downloads',
         fixturesFolder: 'tests/fixtures',
+        screenshotOnRunFailure: false,
         specPattern: 'tests/e2e/**/*.cy.js',
         supportFile: false,
+        video: false,
         videosFolder: 'tests/output/cypress-videos'
     }
 })
